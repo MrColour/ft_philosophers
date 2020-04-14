64b036d465ee4096b63628bc6ad767dee933dd63
kmira <kecolour@gmail.com>
2020-04-14 12:02:40 -0700

    Save work

---
+++ philo_three/includes/philosophers.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xinu <xinu@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/14 11:25:31 by xinu              #+#    #+#             */
+/*   Updated: 2020/04/14 11:29:20 by xinu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERs_H
+# define PHILOSOPHERs_H
+
+# include <pthread.h>
+# include <sys/time.h>
+
+# include <stdio.h>
+
+# include "philosopher_structs.h"
+
+#endif
